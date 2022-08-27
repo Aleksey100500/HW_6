ece1ce4b682227d1cfe8f2befc98bc75170e8786
Aleksey <a_zhuravlyov@bk.ru>
2022-08-28 02:15:12 +0300

    Finished Ex 2

---
+++ Exercise_2/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+
+//[1 2 3 4 5] -> 5 8 3
+
+//[6 7 3 6] -> 36 21
+class Program
+{
+    static void Main (string[] args)
+    {
+        int[] array = {2, 5, 10, 2};
+
+        int mult = 0;
+        for(int i = 0; i < array.Length; i++)
+        {
+            mult = array[i] * array[array.Length - 1 - i];
+
+            if ( i == array.Length / 2)
+            {
+                break;
+            }
+            Console.Write($"{mult}" + " ");
+        }
+    }
+}
