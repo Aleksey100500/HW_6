3189fe8192d33869e0e6dbf37229bd6de3cfc773
Aleksey <a_zhuravlyov@bk.ru>
2022-08-28 01:11:11 +0300

    Ex 1 its a work for lesson 6

--- Exercise_1/Program.cs
+++ Exercise_1/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static int MaxNumberArray (int[] array)
+    {
+        int max = 0;
+        for(int i = 0; i < array.Length; i++)
+        {
+            if ( array[i] > max)
+            {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
+    static int MinNumberArray (int[] array)
+    {
+        int min = array[0];
+        for(int i = 0; i < array.Length; i++)
+        {
+            if ( array[i] < min)
+            {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    static void Main (string[] args)
+    {
+        int[] array = {1, 2, 5, 6, 18, 10, 22, 31};
+        int max = MaxNumberArray(array);
+        int min = MinNumberArray(array);
+        Console.WriteLine(max);
+        Console.WriteLine(min);
+        Console.WriteLine(max - min);
+
+    }
+}
